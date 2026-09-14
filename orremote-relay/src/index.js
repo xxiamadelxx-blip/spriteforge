3c3e86caf02814af44119fb2776039abd9917370
xxiamadelxx-blip <xxiamadelxx@gmail.com>
2026-09-14 14:19:09 +0300

    feat: start Supabase command bus worker

--- orremote-relay/src/index.js
+++ orremote-relay/src/index.js
@@ -1,11 +1,24 @@
 import { loadConfig } from './config.js';
 import { createDeviceRelay } from './device-relay.js';
 import { createRelayServer } from './server.js';
+import { createSupabaseCommandBus } from './supabase-command-bus.js';
 
 const config = loadConfig(process.env);
 const deviceRelay = createDeviceRelay(config);
-const server = createRelayServer(config, deviceRelay);
+const commandBus = createSupabaseCommandBus({ config, deviceRelay });
+const server = createRelayServer(config, deviceRelay, commandBus);
+
+commandBus.start();
 
 server.listen(config.port, config.host, () => {
   console.log(`Ø Remote relay listening on ${config.host}:${config.port}`);
 });
+
+function shutdown() {
+  commandBus.stop();
+  server.close(() => process.exit(0));
+  setTimeout(() => process.exit(0), 2000).unref();
+}
+
+process.once('SIGTERM', shutdown);
+process.once('SIGINT', shutdown);
